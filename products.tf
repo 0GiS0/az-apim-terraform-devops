1e43158a06f3bd22c5fb38c38ee467cba47da413
Gisela <giselatb@outlook.com>
2022-08-04 03:22:09 +0200

    Add policies to products

--- products.tf
+++ products.tf
@@ -4,11 +4,37 @@ resource "azurerm_api_management_product" "freemium" {
   resource_group_name   = azurerm_resource_group.rg.name
   display_name          = "Freemium"
   subscription_required = true
-  subscriptions_limit = 1
+  subscriptions_limit   = 1
   approval_required     = false
   published             = true
-  
+
 }
+
+resource "azurerm_api_management_product_policy" "freemium_policy" {
+  product_id          = azurerm_api_management_product.freemium.product_id
+  resource_group_name = azurerm_resource_group.rg.name
+  api_management_name = azurerm_api_management.apim.name
+
+  xml_content = <<XML
+<policies>
+    <inbound>
+        <quota-by-key calls="5" renewal-period="300" counter-key="@(context.Request.IpAddress)" />
+        <base />
+    </inbound>
+    <backend>
+        <base />
+    </backend>
+    <outbound>
+        <base />
+    </outbound>
+    <on-error>
+        <base />
+    </on-error>
+</policies>
+XML
+
+}
+
 
 resource "azurerm_api_management_product" "basic" {
   product_id            = "basic"
@@ -16,10 +42,36 @@ resource "azurerm_api_management_product" "basic" {
   resource_group_name   = azurerm_resource_group.rg.name
   display_name          = "Basic"
   subscription_required = true
-  subscriptions_limit = 1
+  subscriptions_limit   = 1
   approval_required     = true
   published             = true
-  
+
+}
+
+resource "azurerm_api_management_product_policy" "basic_policy" {
+  product_id          = azurerm_api_management_product.basic.product_id
+  resource_group_name = azurerm_resource_group.rg.name
+  api_management_name = azurerm_api_management.apim.name
+
+  xml_content = <<XML
+<policies>
+    <inbound>
+        <rate-limit calls="5" renewal-period="60" />
+        <quota calls="100" renewal-period="604800" />
+        <base />
+    </inbound>
+    <backend>
+        <base />
+    </backend>
+    <outbound>
+        <base />
+    </outbound>
+    <on-error>
+        <base />
+    </on-error>
+</policies>
+XML
+
 }
 
 resource "azurerm_api_management_product" "standard" {
@@ -28,10 +80,35 @@ resource "azurerm_api_management_product" "standard" {
   resource_group_name   = azurerm_resource_group.rg.name
   display_name          = "Standard"
   subscription_required = true
-  subscriptions_limit = 1
+  subscriptions_limit   = 1
   approval_required     = true
   published             = true
-  
+
+}
+
+resource "azurerm_api_management_product_policy" "standard_policy" {
+  product_id          = azurerm_api_management_product.standard.product_id
+  resource_group_name = azurerm_resource_group.rg.name
+  api_management_name = azurerm_api_management.apim.name
+
+  xml_content = <<XML
+<policies>
+    <inbound>        
+        <quota calls="200" renewal-period="604800" />
+        <base />
+    </inbound>
+    <backend>
+        <base />
+    </backend>
+    <outbound>
+        <base />
+    </outbound>
+    <on-error>
+        <base />
+    </on-error>
+</policies>
+XML
+
 }
 
 resource "azurerm_api_management_product" "premium" {
@@ -40,8 +117,33 @@ resource "azurerm_api_management_product" "premium" {
   resource_group_name   = azurerm_resource_group.rg.name
   display_name          = "Premium"
   subscription_required = true
-  subscriptions_limit = 1
+  subscriptions_limit   = 1
   approval_required     = true
   published             = true
-  
+
+}
+
+resource "azurerm_api_management_product_policy" "premium_policy" {
+  product_id          = azurerm_api_management_product.premium.product_id
+  resource_group_name = azurerm_resource_group.rg.name
+  api_management_name = azurerm_api_management.apim.name
+
+  xml_content = <<XML
+<policies>
+    <inbound>        
+        <quota calls="500" renewal-period="604800" />
+        <base />
+    </inbound>
+    <backend>
+        <base />
+    </backend>
+    <outbound>
+        <base />
+    </outbound>
+    <on-error>
+        <base />
+    </on-error>
+</policies>
+XML
+
 }
