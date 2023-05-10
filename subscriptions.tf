54eaae22742f00572d9ee096f1bbd9caefc3267f
0GiS0 <giselatb@outlook.com>
2023-05-10 12:01:42 +0200

    activate demo subscription

--- subscriptions.tf
+++ subscriptions.tf
@@ -3,9 +3,6 @@ resource "azurerm_api_management_subscription" "demosub" {
   resource_group_name = azurerm_resource_group.rg.name
   # product_id          = azurerm_api_management_product.freemium.product_id
   display_name        = "Demo Subscription"
+  state               = "active"
 }
 
-output "subscription_demo_key"{
-  sensitive = true
-  value = azurerm_api_management_subscription.demosub.primary_key
-}
